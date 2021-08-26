c9e040c86ff4ca8455ba331968ed88a8e74dd28d
Erick Danzer <edanzer@gmail.com>
2021-08-26 16:14:50 -0600

    Get search working home values

--- src/App.js
+++ src/App.js
@@ -2,31 +2,50 @@ import { Component } from 'react'
 import { Jumbotron } from 'react-bootstrap'
 import Search from './components/Search'
 import Results from './components/Results'
+//import * as data from "./data/zillow-home-values"
+import data from "./data/zillow-home-values"
+
+var unirest = require('unirest');
 
 class App extends Component {
+
     state = {
-        resultsList: []
+        activeSearch: false,
+        location: 'Not Specified',
+        averageHomeValue: 'Not Specified',
+        averageRentalPrice: 'Not Specified',
+        grossRentMultiplier: 'Not Specified',
     }
 
     updateResults = (search) => {
-        fetch(`https://dog.ceo/api/breeds/list/all`)
-            .then( response => response.json() )
-            .then( data => {
-                if ( Object.keys(data.message).some( item => search === item ) ) {
-                    fetch(`https://dog.ceo/api/breed/${search}/images/random/5`)
-                        .then( response => response.json() )
-                        .then( data => {
-                            this.setState({ 
-                                resultsList: data.message
-                            });
-                        });
-                } else {
-                    alert('try again - not a valid breed!');
-                }
-            })
-            .catch(function(err) {
-                console.log('There was an error: ', err);
-            });           
+
+        // Get home value data for search, return empty array if no search
+        const regex = new RegExp(search, "i");
+        const results = search ? data.filter( home => home.city.match(regex)) : [];
+        console.log(results);
+        
+        // In case of multiple results, create array of home prices
+        const homePriceArray = [];
+        results.forEach((home) => {
+            console.log('home ', home.homeValue);
+            homePriceArray.push(home.homeValue);
+        });
+        
+        // Calculate average
+        const averagePrice = homePriceArray.reduce( function (sum, value) {
+            return sum + value;
+        }, 0) / homePriceArray.length;
+
+        // Format as currency
+        const formatCurrency = (number) => {
+            return number.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0});
+        }
+
+        // Output
+        this.setState({  activeSearch: true });
+        this.setState({  location: search });
+        this.setState({  averageHomeValue: formatCurrency(averagePrice) });
+        console.log(this.state.activeSearch);
     }
 
     render () {
@@ -37,7 +56,17 @@ class App extends Component {
                     <p>Find the Gross Rent Multiplier for a particular area. Search by zip or city + state.</p>
                     <Search updateSearchResults = {this.updateResults} />
                 </Jumbotron>
-                <Results resultsList = {this.state.resultsList} />
+                {
+                    this.state.activeSearch ? (
+                        <Results 
+                            location = {this.state.location}
+                            averageHomeValue = {this.state.averageHomeValue}
+                            averageRentalPrice = {this.state.averageRentalPrice}
+                            grossRentMultiplier = {this.state.grossRentMultiplier}
+                        />
+                    ) : null
+                }
+                
             </div>    
         );
     }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,14 +1,28 @@
-import { ListGroup } from 'react-bootstrap'
+import { Container, Row, ListGroup } from 'react-bootstrap'
 import { Component } from 'react'
 
 class Results extends Component {
 	render() {
 		return (
-			<ListGroup>
-				{this.props.resultsList.map((item, index) =>
-    				<ListGroup.Item key={index}><img src={item} /></ListGroup.Item>
-				)}
-			</ListGroup>
+			
+			<Container fluid className="results">
+				<Row>
+					<ListGroup className="results-list">
+						<ListGroup.Item className="location">
+							<p><span class="label">Location:</span> {this.props.location}</p>
+						</ListGroup.Item>
+						<ListGroup.Item>
+				  			<p><span class="label">Average Home Value:</span> {this.props.averageHomeValue}</p>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<p><span class="label">Average Rental Price:</span> {this.props.averageRentalPrice}</p>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<p><span class="label">Gross Rent Multiplier:</span> {this.props.grossRentMultiplier}</p>
+						</ListGroup.Item>
+					</ListGroup>
+				</Row>
+			</Container>
 		);
 	}
 }
