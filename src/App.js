b2f00bf58e854908450d477980192a07f35ef2f6
Erick Danzer <edanzer@gmail.com>
2021-08-26 21:33:37 -0600

    Code clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Component } from 'react'
-import { Jumbotron } from 'react-bootstrap'
-import Search from './components/Search'
-import Results from './components/Results'
-import homeValues from "./data/zillow-home-values"
-import rentalPrices from "./data/zillow-rental-prices"
+import { Component } from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import Search from './components/Search';
+import Results from './components/Results';
+import homeValues from './data/zillow-home-values';
+import rentalPrices from './data/zillow-rental-prices';
 
 var unirest = require('unirest');
 
@@ -23,47 +23,43 @@ class App extends Component {
         const regex = new RegExp(search, "i");
         const filteredHomeValues = search ? homeValues.filter( home => home.city.match(regex)) : [];
         const filteredRentalPrices = search ? rentalPrices.filter( rental => rental.cityState.match(regex)) : [];
-        console.log(filteredRentalPrices);
-        
-        // In case of multiple results, create array of all matching home values and rental prices
-        const homeValueArray = [];
-        const rentalPriceArray = [];
-        filteredHomeValues.forEach((home) => {
-            homeValueArray.push(home.homeValue);
-        });
-        filteredRentalPrices.forEach((rental) => {
-            rentalPriceArray.push(rental.rent);
-        });
-        
-        // Calculate averages
-        const averageHomeValue = homeValueArray.reduce( function (sum, value) {
-            return sum + value;
-        }, 0) / homeValueArray.length;
-        const averageRentalPrice = rentalPriceArray.reduce( function (sum, value) {
-            return sum + value;
-        }, 0) / rentalPriceArray.length;
-        console.log('averageRentalPrice ', averageRentalPrice);
 
-        // Format as currency or percentage
-        const formatCurrency = (number) => {
-            return number.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0});
+        // In case of multiple results in one location, calculate average home value and rental price
+        const homeValuesOnly = this.getPricesOnly( filteredHomeValues, 'homeValue' );
+        const rentalPricesOnly = this.getPricesOnly( filteredRentalPrices, 'rent' );
+        const averageHomeValue = this.calculateAverage( homeValuesOnly );
+        const averageRentalPrice = this.calculateAverage( rentalPricesOnly );        
+
+        // Update state
+        this.setState({ activeSearch: true });
+        this.setState({ location: search });
+        this.setState({ averageHomeValue: this.formatNumber('currency', averageHomeValue) });
+        this.setState({ averageRentalPrice: this.formatNumber('currency', averageRentalPrice) });
+        this.setState({ grossRentMultiplier: this.formatNumber('percent', averageRentalPrice/averageHomeValue) });
+
+    }
+
+    getPricesOnly = ( startingArray, propertyName ) => {
+        const newArray = [];
+        for (let i = 0; i < startingArray.length;  i++) {
+            newArray.push( startingArray[i][propertyName]);
         }
-        const formatPercentage = (number) => {
+        return newArray;
+    }
+
+    calculateAverage = ( array ) => {
+        const average = array.reduce( function (sum, value) {
+            return sum + value;
+        }, 0) / array.length;
+        return average;
+    }
+
+    formatNumber = ( type, number ) => {
+        if ( 'currency' == type ) {
+            return number.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0});
+        } else if ( 'percent' == type ) {
             return number.toLocaleString('en-US', { style: 'percent', maximumFractionDigits: 2});
         }
-        var formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-            maximumFractionDigits: 0,      
-        });
-
-        // Output
-        this.setState({  activeSearch: true });
-        this.setState({  location: search });
-        this.setState({  averageHomeValue: formatCurrency(averageHomeValue) });
-        this.setState({  averageRentalPrice: formatCurrency(averageRentalPrice) });
-        this.setState({  grossRentMultiplier: formatPercentage(averageRentalPrice/averageHomeValue) });
-
     }
 
     render () {
