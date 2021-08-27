0e39576ecbad56c2d39df41beea0cd085613fba6
Erick Danzer <edanzer@gmail.com>
2021-08-27 10:06:20 -0600

    Updated components to functional components with hooks

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,30 +1,26 @@
 import { Container, Row, ListGroup } from 'react-bootstrap'
-import { Component } from 'react'
 
-class Results extends Component {
-	render() {
-		return (
-			
-			<Container fluid className="results">
-				<Row>
-					<ListGroup className="results-list">
-						<ListGroup.Item className="location">
-							<p><span className="label">Location:</span> {this.props.location}</p>
-						</ListGroup.Item>
-						<ListGroup.Item>
-				  			<p><span className="label">Average Home Value:</span> {this.props.averageHomeValue}</p>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<p><span className="label">Average Rental Price:</span> {this.props.averageRentalPrice}</p>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<p><span className="label">Gross Rent Multiplier:</span> {this.props.grossRentMultiplier}</p>
-						</ListGroup.Item>
-					</ListGroup>
-				</Row>
-			</Container>
-		);
-	}
+const Results = (props) => {
+	return (
+		<Container fluid className="results">
+			<Row>
+				<ListGroup className="results-list">
+					<ListGroup.Item className="location">
+						<p><span className="label">Location:</span> {props.location}</p>
+					</ListGroup.Item>
+					<ListGroup.Item>
+						<p><span className="label">Average Home Value:</span> {props.averageHomeValue}</p>
+					</ListGroup.Item>
+					<ListGroup.Item>
+						<p><span className="label">Average Rental Price:</span> {props.averageRentalPrice}</p>
+					</ListGroup.Item>
+					<ListGroup.Item>
+						<p><span className="label">Gross Rent Multiplier:</span> {props.grossRentMultiplier}</p>
+					</ListGroup.Item>
+				</ListGroup>
+			</Row>
+		</Container>
+	);
 }
 
 export default Results;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,38 +1,28 @@
-import { Component } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
 
-class Search extends Component {
+const Search = (props) => {
+	const [search, setSearch] = useState('');
 
-	state = {
-		search: ''
+	const handleSubmit = e => {
+		e.preventDefault()
+		props.updateSearchResults(search)
 	}
 
-	handleChange = event => {
-		this.setState({ search: event.target.value });
-	}
-
-	handleSubmit = event => {
-		event.preventDefault()
-		this.props.updateSearchResults(this.state.search)
-	}
-
-	render () {
-		return (
-			<Form inline onSubmit={this.handleSubmit.bind(this)}>
-				<Form.Group controlId="formSearchArea">
-					<Form.Control 
-						type="search" 
-						placeholder="Enter search terms" 
-						className="mr-sm-2"
-						value = {this.state.search}
-						onChange = {this.handleChange}
-              		/>
-				</Form.Group>
-				<Button variant="primary" type="submit">Submit</Button>
-			</Form>
-		)
-	}
+	return (
+		<Form inline onSubmit={handleSubmit.bind(this)}>
+			<Form.Group controlId="formSearchArea">
+				<Form.Control 
+					type="search" 
+					placeholder="Enter search terms" 
+					className="mr-sm-2"
+					value = {search}
+					onChange = {e => setSearch(e.target.value)}
+				/>
+			</Form.Group>
+			<Button variant="primary" type="submit">Submit</Button>
+		</Form>
+	);
 }
 
 export default Search;
-
