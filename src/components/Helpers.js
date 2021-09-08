7f6c480d1c0d44eaf5794f73defd10d95843f3f4
Erick Danzer <edanzer@gmail.com>
2021-09-08 12:39:34 -0600

    Added initial testing coverage with react-testing-library

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -1,7 +1,16 @@
 export const formatNumber = ( type, number ) => {
     if ( 'currency' === type ) {
-        return number.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0});
+        return number.toLocaleString('en-US', { 
+            style: 'currency', 
+            currency: 'USD', 
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0, 
+        });
     } else if ( 'percent' === type ) {
-        return number.toLocaleString('en-US', { style: 'percent', maximumFractionDigits: 2});
+        return number.toLocaleString('en-US', { 
+            style: 'percent', 
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        });
     }
 }
