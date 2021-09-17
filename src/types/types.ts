4d3bccd6eaa84527611b066eb0e12a372ee1058a
Erick Danzer <edanzer@gmail.com>
2021-09-17 14:25:30 -0600

    Moved shared interfaces/types to own module

---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+// export interface AllResults {
+//     value: SingleResult[]
+// }
+
+export interface LocationData {
+    id: number,
+    location: string,
+    averageHomeValue: number,
+    averageRent: number|null,
+    grossRentMultiplier: number|null
+}
+
+export interface LocationCard {
+    id: number,
+    location: string,
+    averageHomeValue: string,
+    averageRent: string,
+    grossRentMultiplier: string
+}
